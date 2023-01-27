5659d7181ca5348933b5bf4970c2723ee5bed349
Ivan Shmakov <xyfos@mail.ru>
2023-01-27 21:17:33 +0300

    3 tasks

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿/* 
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7, min = 5
+a = 2 b = 10 -> max = 10, min = 2 
+*/
+
+/*
+Console.Write("Input first number: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 > number_2)
+{
+    Console.WriteLine("First number is max, second number is min");
+}
+else
+{
+    Console.WriteLine("First number is min, second number is max");
+}
+*/
+
+
+/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+/*
+Console.Write("Input first number: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input third number: ");
+int number_3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number_1;
+
+if (number_2 > max) max = number_2;
+if (number_3 > max) max = number_3;
+
+Console.WriteLine($"Max number is {max}");
+*/
+
+
+/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+/*
+Console.Write("Input your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int a = number % 2;
+
+if (Math.Abs(a) == 1) //взят модуль числа а, проверяется на равенство единице
+{
+    Console.WriteLine("Your number is odd");
+}
+else
+{
+    Console.WriteLine("Your number is even");
+}
+*/
+
+
+/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
